f78602e550d6851956c9212bffad9d7b690706a8
chandu.chandran <chandu.chandran@cesltd.com>
2023-08-17 17:23:41 +0530

    get all employees integration test - failing

---
+++ src/EmployeeManagement/EmployeeManagement.IntegrationTest/ControllerTests/EmployeeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement.IntegrationTest.ControllerTests
+{
+    public class EmployeeControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+        public EmployeeControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ShouldGetEmployeeList_WhenCalled_GetAllEmployeesAsync()
+        {
+            //Act
+            var actual = await _httpClient.GetAsync($"{ApiRoutes.BaseUrl}/{ApiRoutes.Employee}");
+
+            //Assert
+            actual.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
