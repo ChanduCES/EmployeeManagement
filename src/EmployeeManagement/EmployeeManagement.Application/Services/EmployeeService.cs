bee616e145d6927d56425badc544d71f33a1a397
chandu.chandran <chandu.chandran@cesltd.com>
2023-08-24 18:05:50 +0530

    Merging from employee service and repository

---
+++ src/EmployeeManagement/EmployeeManagement.Application/Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using EmployeeManagement.Application.DTO;
+using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Domain.Repositories;
+
+namespace EmployeeManagement.Application.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EmployeesDTO>> GetAllAsync()
+        {
+            List<Employee> employees = await _employeeRepository.GetAllAsync();
+            return _mapper.Map<List<EmployeesDTO>>(employees);
+        }
+    }
+}
---
+++ src/EmployeeManagement/EmployeeManagement.Infrastructure/Repository/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Domain.Repositories;
+using EmployeeManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Infrastructure.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDBContext _context;
+
+        public EmployeeRepository(EmployeeDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllAsync()
+        {
+            List<Employee> employees = await _context.Employees.ToListAsync();
+            return employees;
+        }
+    }
+}
