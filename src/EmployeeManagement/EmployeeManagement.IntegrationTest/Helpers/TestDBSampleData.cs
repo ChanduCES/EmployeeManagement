15c308b27cdd99c55ba904c68075b0218ceea7d3
chandu.chandran <chandu.chandran@cesltd.com>
2023-08-28 17:35:55 +0530

    parameter change for seeding method

--- src/EmployeeManagement/EmployeeManagement.IntegrationTest/Helpers/TestDBSampleData.cs
+++ src/EmployeeManagement/EmployeeManagement.IntegrationTest/Helpers/TestDBSampleData.cs
@@ -7,7 +7,7 @@ namespace EmployeeManagement.IntegrationTests.Helpers
 {
     public static class TestDBSampleData
     {
-        public static List<Address> FetchSeedingAddresses(List<Employee> employees)
+        public static List<Address> FetchSeedingAddresses(int employeeCount)
         {
             var addresses = new Faker<Address>()
                                 .RuleFor(x => x.HouseNo, f => f.Address.BuildingNumber())
@@ -15,7 +15,7 @@ namespace EmployeeManagement.IntegrationTests.Helpers
                                 .RuleFor(x => x.City, f => f.Address.City())
                                 .RuleFor(x => x.State, f => f.Address.State())
                                 .RuleFor(x => x.PostalCode, f => f.Address.ZipCode())
-                                .Generate(employees.Count());
+                                .Generate(employeeCount);
             return addresses;
         }
 
@@ -43,7 +43,7 @@ namespace EmployeeManagement.IntegrationTests.Helpers
             //Adding addresses seed data
             dbContext.Addresses.RemoveRange(dbContext.Addresses);
             var emp = await dbContext.Employees.ToListAsync();
-            var addresses = FetchSeedingAddresses(emp);
+            var addresses = FetchSeedingAddresses(emp.Count());
             dbContext.Addresses.AddRange(addresses);
             dbContext.SaveChanges();
         }
